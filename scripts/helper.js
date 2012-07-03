df325cff34ba9d66b2fbb44e554d7551020be7f9
Girish Sharma <scrapmachines@gmail.com>
2012-07-03 20:42:00 +0530

    License consistency

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,23 +1,6 @@
-/* ***** BEGIN LICENSE BLOCK *****
- *  This is the helper file for addon Search SpyBot
- *  Copyright (C) 2011  Girish Sharma
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/
- *
- *  The original code is the helper and utils file for addon Home Dash.
- *
- * ***** END LICENSE BLOCK ***** */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 "use strict";
 
