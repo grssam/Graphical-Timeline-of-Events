96d198b1aedf72ec176b0b0b8db7c9d0bc637816
Girish Sharma <scrapmachines@gmail.com>
2012-07-21 04:23:10 +0530

    fix type

--- bootstrap.js
+++ bootstrap.js
@@ -107,7 +107,7 @@ function addMenuItem(window) {
       appMenuItem.setAttribute("id", appMenuitemID);
       appMenuItem.setAttribute("type", "checkbox");
       appMenuItem.setAttribute("label", "Graphical Timeline");
-      appMenuitem.setAttribute("accesskey", "G");
+      appMenuItem.setAttribute("accesskey", "G");
       appMenuItem.setAttribute("key", keyID);
       appMenuItem.addEventListener("command", showHideUI);
       appMenu.insertBefore(appMenuItem, $("appmenu_webConsole"));
